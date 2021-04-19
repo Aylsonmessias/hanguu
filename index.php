7ad284f6caffa4061f6948533cbc0879d9f36832
Aylsonmessias <ayllsonme@gmail.com>
2021-04-18 21:40:51 -0300

    teste foto slide01

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
     </ol>
     <div class="carousel-inner" role="listbox">
       <!-- Slide One - Set the background image for this slide in the line below -->
-      <div class="carousel-item active" style="background-image: url('https://github.com/Aylsonmessias/hanguu/blob/master/assets/img/slide/hamburger%20S01.png?raw=true')">
+      <div class="carousel-item active" highlight_file="200px" style="background-image: url('https://github.com/Aylsonmessias/hanguu/blob/master/assets/img/slide/hamburger%20S01.png?raw=true')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">First Slide</h3>
           <p class="lead">This is a description for the first slide.</p>
