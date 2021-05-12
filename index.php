1497efd93be062a2eaba44559317c0992dab3a50
Aylsonmessias <ayllsonme@gmail.com>
2021-05-11 23:00:22 -0300

    add image no index

--- index.php
+++ index.php
@@ -27,31 +27,32 @@
                 </div>
             </div>
         </nav>
-        <!--Slide -->
+        <!--Slide
         <header>
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <!-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
       <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
     </ol>
     <div class="carousel-inner" role="listbox">
-      <!-- Slide One - Set the background image for this slide in the line below -->
+      <!-- Slide One - Set the background image for this slide in the line below 
       <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">First Slide</h3>
           <p class="lead">This is a description for the first slide.</p>
         </div>
       </div>
+
       <!-- Slide Two - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1920x1080')">
+   <!--   <div class="carousel-item" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1920x1080')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">Second Slide</h3>
           <p class="lead">This is a description for the second slide.</p>
         </div>
       </div>
       <!-- Slide Three - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/O7fzqFEfLlo/1920x1080')">
+    <!--  <div class="carousel-item" style="background-image: url('https://source.unsplash.com/O7fzqFEfLlo/1920x1080')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">Third Slide</h3>
           <p class="lead">This is a description for the third slide.</p>
@@ -66,27 +67,32 @@
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="sr-only">Next</span>
         </a>
-  </div>
-</header>
-
-<!-- Page Content -->
-
-<!-- <section class="py-5">
+  </div> -->
+<!--
+ <section class="py-5">
   <div class="container">
     <h1 class="font-weight-light">Half Page Image Slider</h1>
     <p class="lead">The background images for the slider are set directly in the HTML using inline CSS. The images in this snippet are from <a href="https://unsplash.com">Unsplash</a>!</p>
-  </div>
+  </div> 
 </section> -->
 
-        <!-- Masthead-->
-        <!--<header class="masthead">
+ <header class="masthead">
             <div class="container">
                 <div class="masthead-subheading">Bem vindos a melhor plataforma de </div>
                 <div class="masthead-heading text-uppercase">Delivery de Comida</div>
-                <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#contact">Criar Conta</a>
+                <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#contact">Fazer Pedido</a>
             
             </div>
-        </header> -->
+
+</header> 
+
+<!-- Page Content -->
+
+
+
+        <!-- Masthead-->
+       
+        </header> 
 
         <!-- Portfolio Grid-->
         <section class="page-section bg-light" id="portfolio">
