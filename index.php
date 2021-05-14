9e1109681c9b08d3d01139261d012ade895f2569
Lucas <lmamartins23@gmail.com>
2021-05-13 21:06:32 -0300

    LOGINADMIN

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
             <div class="container">
-                <a class="navbar-brand js-scroll-trigger" href="#page-top"><img src="assets/img/logohanguu.svg"/></a>
+                <a class="navbar-brand js-scroll-trigger"  href="#page-top"><img src="assets/img/logohanguu.svg"/></a>
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                     Menu1
                     <i class="fas fa-bars ml-1"></i>
