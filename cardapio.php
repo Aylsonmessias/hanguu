ec0c6e681efb987cc43bc1e54db6514d91be26c1
Aylsonmessias <ayllsonme@gmail.com>
2021-06-04 20:15:05 -0300

    ajustando tamanhos fotos slide

--- cardapio.php
+++ cardapio.php
@@ -65,7 +65,7 @@
     </ol>
     <div class="carousel-inner" role="listbox">
       <!-- Slide One - Set the background image for this slide in the line below --> 
-      <div class="carousel-item active" style="background-image: url('https://source.unsplash.com/RCAhiGJsUUE/1920x1080')">
+      <div class="carousel-item active" style="background-image: url('https://github.com/Aylsonmessias/hanguu/blob/master/assets/img/slide/slide1.png?raw=true/1920x1080')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">First Slide</h3>
           <p class="lead">This is a description for the first slide.</p>
@@ -73,14 +73,14 @@
       </div>
 
       <!-- Slide Two - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/wfh8dDlNFOk/1920x1080')">
+      <div class="carousel-item" style="background-image: url('https://github.com/Aylsonmessias/hanguu/blob/master/assets/img/slide/slide2.png?raw=true/1920x1080')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">Second Slide</h3>
           <p class="lead">This is a description for the second slide.</p>
         </div>
       </div>
       <!-- Slide Three - Set the background image for this slide in the line below -->
-      <div class="carousel-item" style="background-image: url('https://source.unsplash.com/O7fzqFEfLlo/1920x1080')">
+      <div class="carousel-item" style="background-image: url('https://github.com/Aylsonmessias/hanguu/blob/master/assets/img/slide/slide3.png?raw=true/1920x1080')">
         <div class="carousel-caption d-none d-md-block">
           <h3 class="display-4">Third Slide</h3>
           <p class="lead">This is a description for the third slide.</p>
